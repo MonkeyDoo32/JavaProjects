d13d363ca68703a826bfb4cf6f9037fdecfd7b62
MonkeyDoo32 <167645700+MonkeyDoo32@users.noreply.github.com>
2024-04-20 15:23:53 -0500

    init commit

---
+++ HelloWord.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class HelloWord {
+
+
+    public static void main(String[] args) {
+
+        //Scanner
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello World!!");
+    }
+}
